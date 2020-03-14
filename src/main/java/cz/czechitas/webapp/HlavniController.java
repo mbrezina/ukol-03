dea0429fe0d32f73799bcbd4a98cf1d9fc37f98e
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-03-14 17:13:09 +0100

    lists of objects as finals

--- src/main/java/cz/czechitas/webapp/HlavniController.java
+++ src/main/java/cz/czechitas/webapp/HlavniController.java
@@ -10,34 +10,47 @@ import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.*;
+
 @Controller
 public class HlavniController {
+
+    List <Polozka> polozkyKosiku = new Arraylist<>();
+
+    private static final List<kartaPexesa> SEZNAMKARTICEK = asList(
+            new kartaPexesa("images/amal.jpg", "Amálka", 1),
+            new kartaPexesa("images/amal.jpg", "Amálka", 2),
+            new kartaPexesa("images/carod.jpg", "Malá Čarodějnice", 1),
+            new kartaPexesa("images/carod.jpg", "Malá Čarodějnice", 2),
+            new kartaPexesa("images/mak.jpg", "Maková panenka", 1),
+            new kartaPexesa("images/mak.jpg", "Maková panenka", 2),
+            new kartaPexesa("images/rakos.jpg", "Rákosníček", 1),
+            new kartaPexesa("images/rakos.jpg", "Rákosníček", 2),
+            new kartaPexesa("images/rum.jpg", "Rumcajs", 1),
+            new kartaPexesa("images/rum.jpg", "Rumcajs", 2),
+            new kartaPexesa("images/vecer.jpg", "Večerníček", 1),
+            new kartaPexesa("images/vecer.jpg", "Večerníček", 2),
+            new kartaPexesa("images/staf.jpg", "Štaflík", 1),
+            new kartaPexesa("images/staf.jpg", "Štaflík", 2),
+            new kartaPexesa("images/sebes.jpg", "Šebestová", 1),
+            new kartaPexesa("images/sebes.jpg", "Šebestová", 2)
+    );
+
+    private static final List<Hrac> SEZNAMHRACU = asList(
+            new Hrac("Pepina", "Nováková", "kuk@gmail.com", 1, 20, 0),
+            new Hrac("Jolka", "Krásná", "jolka@gmail.com", 9, 20, 0),
+            new Hrac("Jiřina", "Trávníková", "jirina@gmail.com", 2, 20, 0),
+            new Hrac("Jana", "Kludská", "kludsjanagmail.com", 4, 20, 0),
+            new Hrac("Ivana", "Opršálková", "ivop@gmail.com", 5, 20, 0)
+    );
+
 
     @RequestMapping({"/", "/index.html"})
     public ModelAndView zobrazIndex() {
 
         ModelAndView drzak = new ModelAndView("index");
-
-        List<kartaPexesa> seznamKarticek = new ArrayList<>();
-        seznamKarticek.add(new kartaPexesa("images/amal.jpg","Amálka", 1));
-        seznamKarticek.add(new kartaPexesa("images/amal.jpg","Amálka", 2));
-        seznamKarticek.add(new kartaPexesa("images/carod.jpg","Malá Čarodějnice", 1));
-        seznamKarticek.add(new kartaPexesa("images/carod.jpg","Malá Čarodějnice", 2));
-        seznamKarticek.add(new kartaPexesa("images/mak.jpg","Maková panenka", 1));
-        seznamKarticek.add(new kartaPexesa("images/mak.jpg","Maková panenka", 2));
-        seznamKarticek.add(new kartaPexesa("images/rakos.jpg","Rákosníček", 1));
-        seznamKarticek.add(new kartaPexesa("images/rakos.jpg","Rákosníček", 2));
-        seznamKarticek.add(new kartaPexesa("images/rum.jpg","Rumcajs", 1));
-        seznamKarticek.add(new kartaPexesa("images/rum.jpg","Rumcajs", 2));
-        seznamKarticek.add(new kartaPexesa("images/vecer.jpg","Večerníček", 1));
-        seznamKarticek.add(new kartaPexesa("images/vecer.jpg","Večerníček", 2));
-        seznamKarticek.add(new kartaPexesa("images/staf.jpg","Štaflík", 1));
-        seznamKarticek.add(new kartaPexesa("images/staf.jpg","Štaflík", 2));
-        seznamKarticek.add(new kartaPexesa("images/sebes.jpg","Šebestová", 1));
-        seznamKarticek.add(new kartaPexesa("images/sebes.jpg","Šebestová", 2));
-
-        Collections.shuffle(seznamKarticek);
-        drzak.addObject("balicekPexesa", seznamKarticek);
+        Collections.shuffle(SEZNAMKARTICEK);
+        drzak.addObject("balicekPexesa", SEZNAMKARTICEK);
         drzak.addObject("datum", LocalDateTime.now());
         return drzak;
     }
@@ -46,21 +59,13 @@ public class HlavniController {
     public ModelAndView zobrazHrace() {
 
         ModelAndView drzak = new ModelAndView("hraci");
-
-        List<Hrac> seznamHracu = new ArrayList<>();
-        seznamHracu.add(new Hrac("Pepina" ,"Nováková" ,"kuk@gmail.com", 1, 20, 0));
-        seznamHracu.add(new Hrac("Jolka" ,"Krásná" ,"jolka@gmail.com", 9, 20, 0));
-        seznamHracu.add(new Hrac("Jiřina" ,"Trávníková" ,"jirina@gmail.com", 2, 20, 0));
-        seznamHracu.add(new Hrac("Jana" ,"Kludská" ,"kludsjanagmail.com", 4, 20, 0));
-        seznamHracu.add(new Hrac("Ivana" ,"Opršálková" ,"ivop@gmail.com", 5, 20, 0));
-
-        seznamHracu.sort(Comparator.comparing(Hrac::getPocetVyhranychTurnaju).reversed());
-        for (int i = 0; i < seznamHracu.size(); i++) {
-            seznamHracu.get(i).setPoradi(++i);
+        SEZNAMHRACU.sort(Comparator.comparing(Hrac::getPocetVyhranychTurnaju).reversed());
+        for (int i = 0; i < SEZNAMHRACU.size(); i++) {
+            SEZNAMHRACU.get(i).setPoradi(++i);
             --i;
         }
 
-        drzak.addObject("zebricekHracu", seznamHracu);
+        drzak.addObject("zebricekHracu", SEZNAMHRACU);
         drzak.addObject("datum", LocalDateTime.now());
         return drzak;
 
